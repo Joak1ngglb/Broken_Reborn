82952501096d3375ebc54712fb778a377065121f
BlestroKain <109327190+BlestroKain@users.noreply.github.com>
2025-12-02 10:00:48 -0500

    Apply enchantment bonuses to weapon damage (#693)

--- Framework/Intersect.Framework.Core/GameObjects/Items/ItemProperties.cs
+++ Framework/Intersect.Framework.Core/GameObjects/Items/ItemProperties.cs
@@ -18,20 +18,23 @@ public partial class ItemProperties
         }
 
         EnchantmentLevel = other.EnchantmentLevel;
+        BaseDamageModifier = other.BaseDamageModifier;
         Array.Copy(other.StatModifiers, StatModifiers, Enum.GetValues<Stat>().Length);
         Array.Copy(other.VitalModifiers, VitalModifiers, Enum.GetValues<Vital>().Length);
-      
+
     }
 
     [Key(0)]
     public int[] StatModifiers { get; set; } = new int[Enum.GetValues<Stat>().Length];
 
     [Key(1)]
-    public int EnchantmentLevel { get; set; }  // Nivel de encantamiento  
+    public int EnchantmentLevel { get; set; }  // Nivel de encantamiento
     [Key(2)]
     public Dictionary<int, int[]> EnchantmentRolls { get; set; } = new Dictionary<int, int[]>();
     [Key(3)]
     public int[] VitalModifiers { get; set; } = new int[Enum.GetValues<Vital>().Length];
     [Key(4)]
     public int MageSink { get;  set; }
+    [Key(5)]
+    public int BaseDamageModifier { get; set; }
 }
