76d5189c8e809892696845b2f8d58c10a64ca3df
BlestroKain <jlrootsloud@gmail.com>
2025-12-09 23:36:37 -0500

    Update MenuContainer.cs

--- Intersect.Client.Core/Interface/Game/MenuContainer.cs
+++ Intersect.Client.Core/Interface/Game/MenuContainer.cs
@@ -9,7 +9,6 @@ using Intersect.Client.Interface.Game.Character;
 using Intersect.Client.Interface.Game.Chat;
 using Intersect.Client.Interface.Game.Inventory;
 using Intersect.Client.Interface.Game.Job;
-using Intersect.Client.Interface.Game.Map;
 using Intersect.Client.Interface.Game.Spells;
 using Intersect.Client.Localization;
 using Intersect.Client.Networking;
