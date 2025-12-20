d3ecd6f303ae40b253587e9f7e109e51010c4293
BlestroKain <109327190+BlestroKain@users.noreply.github.com>
2025-12-20 10:49:49 -0500

    Add fishing input definitions

---
+++ Framework/Intersect.Framework.Core/Fishing/FishingInput.cs
@@ -0,0 +1,22 @@
+namespace Intersect.Fishing;
+
+[System.Flags]
+public enum FishingInputFlags
+{
+    None = 0,
+    Tap = 1 << 0,
+}
+
+public struct FishingInputFrame
+{
+    public int TickMs { get; init; }
+
+    public FishingInputFlags Flags { get; init; }
+}
+
+public enum FishingSimResult
+{
+    InProgress,
+    Success,
+    Failed,
+}
