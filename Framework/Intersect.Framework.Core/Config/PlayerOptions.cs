b8e51c21e9a37ea93c29989e97201e1ac52abd56
BlestroKain <109327190+BlestroKain@users.noreply.github.com>
2026-01-21 18:15:17 -0500

    Add DeathSeconds to player options (#887)

--- Framework/Intersect.Framework.Core/Config/PlayerOptions.cs
+++ Framework/Intersect.Framework.Core/Config/PlayerOptions.cs
@@ -48,6 +48,11 @@ public partial class PlayerOptions
     public int ExpLossOnDeathPercent { get; set; } = 0;
 
     /// <summary>
+    /// Time in seconds before a player can respawn after death.
+    /// </summary>
+    public int DeathSeconds { get; set; } = 0;
+
+    /// <summary>
     /// Percentage between 0 and 100 applied weekly to move player honor toward zero.
     /// </summary>
     public int HonorDecayPercent { get; set; } = 0;
