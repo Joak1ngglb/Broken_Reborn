dd06fc023a97583128abe885cc6ab1e7cbc725fc
BlestroKain <109327190+BlestroKain@users.noreply.github.com>
2026-02-09 20:37:35 -0500

    feat: render action damage messages with digit textures

--- Intersect.Client.Core/Maps/ActionMessage.cs
+++ Intersect.Client.Core/Maps/ActionMessage.cs
@@ -1,4 +1,7 @@
-﻿using Intersect.Client.Framework.Maps;
+﻿using System.Text;
+using Intersect.Client.Framework.Content;
+using Intersect.Client.Framework.Graphics;
+using Intersect.Client.Framework.Maps;
 using Intersect.Client.General;
 using Intersect.Framework.Core;
 using Intersect.Utilities;
@@ -8,6 +11,13 @@ namespace Intersect.Client.Maps;
 
 public partial class ActionMessage : IActionMessage
 {
+    private const int DIGIT_SPACING = 0;
+    private const int DIGIT_WIDTH = 9;
+    private const int DIGIT_HEIGHT = 12;
+
+    private readonly List<IGameTexture> _digitTextures = [];
+    private bool _texturesLoaded;
+
     public Color Color { get; init; }
 
     public IMapInstance Map { get; init; }
@@ -33,10 +43,84 @@ public partial class ActionMessage : IActionMessage
         TransmissionTimer = Timing.Global.MillisecondsUtc + 1000;
     }
 
+    public void Draw(int mapX, int mapY, int tileWidth, int tileHeight)
+    {
+        var x = mapX + X * tileWidth + XOffset;
+        var y = mapY + Y * tileHeight - tileHeight * 2 * (1000 - (int)(TransmissionTimer - Timing.Global.MillisecondsUtc)) / 1000;
+
+        if (!_texturesLoaded)
+        {
+            _digitTextures.Clear();
+            var numberBuilder = new StringBuilder();
+            foreach (var character in Text)
+            {
+                if (char.IsDigit(character))
+                {
+                    numberBuilder.Append(character);
+                }
+            }
+
+            var numericText = numberBuilder.ToString();
+            foreach (var character in numericText)
+            {
+                var texture = Globals.ContentManager.GetTexture(TextureType.Misc, $"{character}.png");
+                if (texture != null)
+                {
+                    _digitTextures.Add(texture);
+                }
+            }
+
+            _texturesLoaded = true;
+        }
+
+        if (_digitTextures.Count > 0)
+        {
+            var totalWidth = _digitTextures.Count * DIGIT_WIDTH + (_digitTextures.Count - 1) * DIGIT_SPACING;
+            var startX = x - totalWidth / 2f;
+
+            for (var i = 0; i < _digitTextures.Count; i++)
+            {
+                var texture = _digitTextures[i];
+                var drawX = startX + i * (DIGIT_WIDTH + DIGIT_SPACING);
+
+                Graphics.Renderer.DrawTexture(
+                    texture,
+                    0,
+                    0,
+                    texture.Width,
+                    texture.Height,
+                    drawX,
+                    y,
+                    DIGIT_WIDTH,
+                    DIGIT_HEIGHT,
+                    Color.White
+                );
+            }
+
+            return;
+        }
+
+        var textWidth = Graphics.Renderer.MeasureText(Text, Graphics.ActionMsgFont, Graphics.ActionMsgFontSize, 1).X;
+        Graphics.Renderer.DrawString(
+            Text,
+            Graphics.ActionMsgFont,
+            Graphics.ActionMsgFontSize,
+            x - textWidth / 2f,
+            y,
+            1,
+            Color,
+            true,
+            null,
+            new Color(40, 40, 40)
+        );
+    }
+
     public void TryRemove()
     {
         if (TransmissionTimer <= Timing.Global.MillisecondsUtc)
         {
+            _digitTextures.Clear();
+            _texturesLoaded = false;
             (Map as MapInstance)?.ActionMessages.Remove(this);
         }
     }
