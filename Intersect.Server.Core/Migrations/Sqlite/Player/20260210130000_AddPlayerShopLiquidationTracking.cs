2d01bcc23eba30b5731bc93a744b89da22ec78bd
BlestroKain <109327190+BlestroKain@users.noreply.github.com>
2026-02-10 12:22:40 -0500

    Add expired player shop liquidation on login (#1027)

---
+++ Intersect.Server.Core/Migrations/Sqlite/Player/20260210130000_AddPlayerShopLiquidationTracking.cs
@@ -0,0 +1,66 @@
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Intersect.Server.Migrations.Sqlite.Player
+{
+    public partial class AddPlayerShopLiquidationTracking : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            switch (migrationBuilder.ActiveProvider)
+            {
+                case "Microsoft.EntityFrameworkCore.Sqlite":
+                    migrationBuilder.AddColumn<DateTime>(
+                        name: "LiquidatedAt",
+                        table: "Player_Shops",
+                        type: "TEXT",
+                        nullable: true
+                    );
+
+                    migrationBuilder.AddColumn<long>(
+                        name: "LiquidatedGold",
+                        table: "Player_Shops",
+                        type: "INTEGER",
+                        nullable: false,
+                        defaultValue: 0L
+                    );
+                    break;
+
+                case "Pomelo.EntityFrameworkCore.MySql":
+                    migrationBuilder.AddColumn<DateTime>(
+                        name: "LiquidatedAt",
+                        table: "Player_Shops",
+                        type: "datetime(6)",
+                        nullable: true
+                    );
+
+                    migrationBuilder.AddColumn<long>(
+                        name: "LiquidatedGold",
+                        table: "Player_Shops",
+                        type: "bigint",
+                        nullable: false,
+                        defaultValue: 0L
+                    );
+                    break;
+
+                default:
+                    throw new NotSupportedException(migrationBuilder.ActiveProvider);
+            }
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LiquidatedAt",
+                table: "Player_Shops"
+            );
+
+            migrationBuilder.DropColumn(
+                name: "LiquidatedGold",
+                table: "Player_Shops"
+            );
+        }
+    }
+}
